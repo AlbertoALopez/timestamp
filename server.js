68769db27e5f0898506f32ef7e81b6b036be1c4d
Alberto Lopez <albertolopez19@gmail.com>
2016-08-26 11:12:09 -0400

    Add nunjucks templating and basic index.njs

--- server.js
+++ server.js
@@ -1,2 +1,25 @@
 const express = require('express');
+const http = require('http');
+const path = require('path');
+const nunjucks = require('nunjucks');
 const app = express();
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'njs');
+
+app.get('/', (req, res) => {
+    res.render('pages/index.html');
+});
+
+app.listen(3000, () => {
+    console.log('Running node server');
+});
